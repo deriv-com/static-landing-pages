ca44865760deeb9c864076e83f537a71ed7dfa94
amir ali <amir.ali@regentmarkets.com>
2023-10-24 12:27:29 +0500

    add favicon support

--- scripts/insert-string.js
+++ scripts/insert-string.js
@@ -1,9 +1,8 @@
-
 const crc32 = require("crc-32").str;
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const insertStringsToHtmlFile = (input_file, translation_object, lang) => {
+const insertStringsToHtmlFile = (input_file, translation_object, lang, favicon) => {
   const dom = new JSDOM(input_file);
 
   const document = dom.window.document;
@@ -12,8 +11,17 @@ const insertStringsToHtmlFile = (input_file, translation_object, lang) => {
     "element__translation_string__element"
   );
 
+  // if favicon is true, add a link tag with favicon href
+  if (favicon) {
+    const link = document.createElement("link");
+    link.rel = "icon";
+    link.type = "image/png";
+    link.href = "../assets/favicon.png";
+    document.head.appendChild(link);
+  }
+
   // set html lang attribute to the language provided
-    document.documentElement.setAttribute("lang", lang);
+  document.documentElement.setAttribute("lang", lang);
 
   Array.from(el_translations).forEach((el_translation) => {
     const translation_str = el_translation.innerHTML
@@ -21,18 +29,18 @@ const insertStringsToHtmlFile = (input_file, translation_object, lang) => {
       : "";
 
     if (translation_str) {
-        const translationHash = crc32(translation_str);
+      const translationHash = crc32(translation_str);
 
-        if (translation_object[translationHash]) {
-            el_translation.innerHTML = translation_object[translationHash];
-        }
+      if (translation_object[translationHash]) {
+        el_translation.innerHTML = translation_object[translationHash];
+      }
     }
 
     el_translation.classList.remove("element__translation_string__element");
 
     // remove the class attribute if the value is empty string
     if (!el_translation.getAttribute("class")) {
-        el_translation.removeAttribute("class");
+      el_translation.removeAttribute("class");
     }
   });
 
